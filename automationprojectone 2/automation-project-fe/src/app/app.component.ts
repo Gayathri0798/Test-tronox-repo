f56d8d77fc0da6046481bc73db2549cbe280e7a7
Lav O Pandey <lav.pandey@in.ey.com>
2025-03-19 13:44:12 +0530

    VNC

--- automationprojectone 2/automation-project-fe/src/app/app.component.ts
+++ automationprojectone 2/automation-project-fe/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { VncViewerComponent } from './vnc-viewer/vnc-viewer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [VncViewerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
