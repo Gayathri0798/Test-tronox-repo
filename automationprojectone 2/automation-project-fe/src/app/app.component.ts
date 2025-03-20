b53eab23da0d6a15b50765b432e7a4a2a35c2126
Lav O Pandey <lav.pandey@in.ey.com>
2025-03-20 14:55:28 +0530

    stream

--- automationprojectone 2/automation-project-fe/src/app/app.component.ts
+++ automationprojectone 2/automation-project-fe/src/app/app.component.ts
@@ -11,20 +11,25 @@ import { AppService } from './app.service';
 export class AppComponent {
   title = 'automation-project-fe';
   disableButton = false;
+  videoUrl: string | null = null;
 
   constructor(private appService: AppService) {}
 
   onRunTestClick() {
     this.disableButton = true;
+    this.videoUrl = null; // Reset video before running test
+
     this.appService.runTest().subscribe({
-      next: (data: any) => {
-        if (data) {
+      next: () => {
+        // Delay setting video URL slightly to ensure it's available
+        setTimeout(() => {
+          this.videoUrl = 'http://34.93.172.107:3000/test-video';
           this.disableButton = false;
-        }
+        }, 3000);
       },
       error: (err) => {
         this.disableButton = false;
-        console.log('Error', err);
+        console.log('Error running test:', err);
       },
     });
   }
