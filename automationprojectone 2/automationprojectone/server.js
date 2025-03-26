d6ce5c5ee2f7ed9d64b579f5b9cca975e12d5087
Gayathri0798 <gayathri.subbiah@in.ey.com>
2025-03-26 17:04:11 +0530

    dist changes

--- automationprojectone 2/automationprojectone/server.js
+++ automationprojectone 2/automationprojectone/server.js
@@ -170,6 +170,15 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; style-src 'self';"
+  );
+  next();
+});
+
+
 const io = new Server(server, {
   cors: corsOptions,
 });
